06abe4658eeab8bdd4395912a88b776c4730ea5d
Jahanzaib00 <88446738+Jahanzaib00@users.noreply.github.com>
2022-06-30 15:31:47 +0500

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+const url = "https://course-api.com/react-tours-project";
 
-function App() {
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const getData = async () => {
+    setLoading(true);
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => setData(data))
+      .catch();
+    setLoading(false);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  const clickHandler = (id) => {
+    const newData = data.filter((tour) => tour.id !== id);
+    setData(newData);
+  };
+  if (loading) {
+    return (
+      <>
+        <h1>Loading....</h1>
+      </>
+    );
+  }
+  if (data.length === 0) {
+    return (
+      <div className="text-center">
+        <h1>No Tour Left</h1>
+        <button className="btn btn-primary" onClick={getData}>
+          Refresh
+        </button>
+      </div>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="text-center">Our Tours</h1>
+      {data.map(({ id, image, info, name, price }) => {
+        console.log(id);
+        return (
+          <div key={id}>
+            <div className="card">
+              <img className="card-img-top" src={image} alt="" />
+              <div className="card-body">
+                <h5 className="card-title">
+                  {name}
+                  <span className="float-right" style={{ color: "red" }}>
+                    Rs {price}
+                  </span>
+                </h5>
+                <p className="card-text">{info}</p>
+                <button
+                  className="btn btn-primary"
+                  onClick={() => clickHandler(id)}
+                >
+                  Not interested
+                </button>
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
